017465fe21eef8d26d4bc3858310378e46197a83
flcz163 <lf306026038@163.com>
2017-01-08 10:24:08 +0800

    测试 android  Activity

---
+++ app/src/main/java/com/example/testing/testingexample/Calculator.java
@@ -0,0 +1,22 @@
+package com.example.testing.testingexample;
+
+/**
+ * Created by Administrator on 2017/1/6 0006.
+ */
+public class Calculator {
+    public double sum(double a,double b){
+        return a+b;
+    }
+
+    public double substract(double a, double b){
+        return a-b;
+    }
+
+    public  double divide(double a, double b){
+        return a/b;
+    }
+
+    public double multiply(double a, double b){
+        return  a*b;
+    }
+}
